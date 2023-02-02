e2187a79131851ae6a211111438e5182954c31a5
Auliya Apriwiguna <45688720+auliyaapri@users.noreply.github.com>
2023-02-02 17:36:43 +0700

    Delete stockbarang directory

--- stockbarang/cek.php
+++
@@ -1,9 +0,0 @@
-<?php
-    # Jika belum login, maka tidak bisa langsung ketik index.php di url
-    if (isset($_SESSION['log'])) {
-        
-    } else {
-        header('location:login.php');
-    }
-
-?>
